9f46a4d3aebbf1d5832816405e8d083517b2a884
Serch <ztorre23@gmail.com>
2020-09-25 15:53:14 +0200

    debugg some errors

--- js/script.js
+++ js/script.js
@@ -3,29 +3,23 @@ function newItem () {
   //jQuery
   // 1. Adding a new item to the list of items:
   let li = $('<li></li>');
-  let inputValue = $('input').val();
+  let inputValue = $('#input').val();
   li.append(inputValue);
 
   if (inputValue === '') {
-    alert("You must write something")
+    alert("You must write something");
   } else {
-    $('list')append(li);
+    $('#list').append(li);
   }
 
   // 2. Crossing out an item from the list of items:
-  function crossOut() {
-   li.classList.toggle("strike");
-  }
-
-  li.addEventListener("dblclick",crossOut);
-
   function crossOut() {
     li.toggleClass('strike');
   }
 
   li.on("dbclick", function crossOut() {
     li.toggleClass('strike');
-  })
+  });
 
   // 3. Adding the delete button "X":
   let crossOutButton = $('<crossOutButton></crossOutButton>');
@@ -39,6 +33,5 @@ function newItem () {
   }
 
    // 5. Reordering the items:
-
-
+   $('#list').sortable();
 }
