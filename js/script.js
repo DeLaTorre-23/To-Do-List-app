252170fd0ab81ce09b53bce286627a9041dad1a1
Sergio De La Torre <ztorre23@gmail.com>
2021-05-27 23:04:34 +0200

    Update README

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-function newItem() {
+function newItem () {
+
   //jQuery
   // 1. Adding a new item to the list of items:
   let li = $('<li></li>');
@@ -6,7 +7,7 @@ function newItem() {
   li.append(inputValue);
 
   if (inputValue === '') {
-    alert('You must write something');
+    alert("You must write something");
   } else {
     $('#list').append(li);
   }
@@ -16,19 +17,21 @@ function newItem() {
     li.toggleClass('strike');
   }
 
-  li.on('dbclick', crossOut);
+  li.on("dbclick", function crossOut() {
+    li.toggleClass('strike');
+  });
 
   // 3. Adding the delete button "X":
   let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append('X');
+  crossOutButton.append("X");
   li.append(crossOutButton);
 
   // 4. Adding CLASS DELETE (DISPLAY: NONE) from the css:
-  crossOutButton.on('click', deleteListItem);
+  crossOutButton.on("click", deleteListItem);
   function deleteListItem() {
-    li.addClass('delete');
+    li.addClass("delete")
   }
 
-  // 5. Reordering the items:
-  $('#list').sortable();
+   // 5. Reordering the items:
+   $('#list').sortable();
 }
